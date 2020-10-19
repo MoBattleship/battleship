8d695fa10ecd0b1c30ee5823f79dccb16264dd74
arrizalrahmat <arrizalrahmat@gmail.com>
2020-10-19 22:41:26 +0700

    handle disabled selected color

--- client/src/pages/LobbyPage/components/PlayerCard.js
+++ client/src/pages/LobbyPage/components/PlayerCard.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Card, Button } from "react-bootstrap";
+import socket from "../../../helpers/socket";
 
-function PlayerCard({ player }) {
-  const [playerColour, setPlayerColour] = useState(player.color);
+function PlayerCard({ player, playerColours }) {
   const colours = [
     "#f9ca24",
     "#e84118",
@@ -12,39 +12,31 @@ function PlayerCard({ player }) {
     "#fff200",
     "#f8a5c2",
   ];
+  let newColour = []
+  // playerColours = playerColours.forEach(color => {
+  //   console.log(colours.includes(color.color), 'ini color.color')
+  //   if(!colours.includes(color.color)) {
+  //     newColour.push(colours[colours.indexOf(color.color)])
+  //   }
+  // })
+  for (let i = 0; i < colours.length; i++) {
+    for (let j = 0; j < playerColours.length; j++) {
+      if (colours[i] !== playerColours[j]) {
+        newColour.push(colours[i])
+      }
+    }
+  }
+  console.log(newColour, 'ini newColour')
+  console.log(playerColours, 'ini playerColours')
+  console.log(player, 'ini player di card')
+  console.log(player.socketId, 'ini player.socketId')
+  console.log(socket.id, 'ini socket.id')
+  const [playerColour, setPlayerColour] = useState(player.color);
 
   function playerColourGenerator(selectedColour) {
     setPlayerColour(selectedColour);
     console.log(playerColour, "ini playercolour");
   }
-
-  // function buttonFunctionality(selectedColour) {
-  //   if (playerColour === selectedColour) {
-  //     console.log('masuk if')
-  //     return (
-  //       <Button
-  //         key={index}
-  //         className="mr-1"
-  //         style={{ background: colour, height: "5px", width: "5px" }}
-  //         value={colour}
-  //         disabled={true}
-  //         onClick={() => playerColourGenerator(colour)}
-  //       ></Button>
-  //     )
-  //   } else {
-  //     console.log('masuk else')
-  //     return (
-  //       <Button
-  //         key={index}
-  //         className="mr-1"
-  //         style={{ background: colour, height: "5px", width: "5px" }}
-  //         value={colour}
-  //         disabled={false}
-  //         onClick={() => playerColourGenerator(colour)}
-  //       ></Button>
-  //     )
-  //   }
-  // }
 
   return (
     <div>
@@ -60,36 +52,40 @@ function PlayerCard({ player }) {
           </div>
           <Card.Body>
             <Card.Title>{player.name}</Card.Title>
-            <div className="row justify-content-left">
-              <Card.Text>Select Color:</Card.Text>
-            </div>
-            <div className="row justify-content-left">
-              {colours.map((colour, index) => {
-                if(colour === playerColour) {
-                  return (
-                    <Button
-                      key={index}
-                      className="mr-1"
-                      style={{ background: colour, height: "5px", width: "5px" }}
-                      value={colour}
-                      disabled={true}
-                      onClick={() => playerColourGenerator(colour)}
-                  ></Button>
-                  );
-                } else {
-                  return (
-                    <Button
-                      key={index}
-                      className="mr-1"
-                      style={{ background: colour, height: "5px", width: "5px" }}
-                      value={colour}
-                      disabled={false}
-                      onClick={() => playerColourGenerator(colour)}
+            <Card.Text>Select Color:</Card.Text>
+            {socket.id === player.socketId && 
+            <div>
+              <div className="row justify-content-left">
+              </div>
+              <div className="row justify-content-left">
+                {colours.map((colour, index) => {
+                  if(colour === playerColour) {
+                    return (
+                      <Button
+                        key={index}
+                        className="mr-1"
+                        style={{ background: colour, height: "5px", width: "5px" }}
+                        value={colour}
+                        disabled={true}
+                        onClick={() => playerColourGenerator(colour)}
                     ></Button>
-                  )
-                }
-              })}
+                    );
+                  } else {
+                    return (
+                      <Button
+                        key={index}
+                        className="mr-1"
+                        style={{ background: colour, height: "5px", width: "5px" }}
+                        value={colour}
+                        disabled={false}
+                        onClick={() => playerColourGenerator(colour)}
+                      ></Button>
+                    )
+                  }
+                })}
+              </div>
             </div>
+            }
           </Card.Body>
         </Card>
       </div>
