a9bd2a3aa2d8cf2d92e155d4778341b7f163bfee
juanbrundism <juanbrundism@gmail.com>
2020-10-19 13:42:52 +0700

    Dropable finish

--- client/src/pages/GamePage/index.js
+++ client/src/pages/GamePage/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 
 import Board2Players from './components/Board2Players'
 import Board3Players from './components/Board3Players'
@@ -6,16 +6,26 @@ import Board4Players from './components/Board4Players'
 import PlayerComputer from './components/PlayerComputer'
 
 function GamePage() {
+  const [display, setDisplay] = useState('')
+
+  useEffect(() => {
+    
+  }, [display])
+  const handleSetDisplay = (data) => {
+    setDisplay(data)
+  }
   return (
     <div className="row m-5 ">
       <div style={{width: "40%"}}>
-        <PlayerComputer />
+        {
+          display ? <PlayerComputer changeDisplay={display}/> : <PlayerComputer />
+        }
       </div>
       <div style={{width: "60%"}}>
-        <Board2Players />
+        {/* <Board2Players handleDisplay={handleSetDisplay}/> */}
+        {/* <Board3Players handleDisplay={handleSetDisplay}/> */}
+        <Board4Players handleDisplay={handleSetDisplay}/>
       </div>
-      {/* <Board3Players />
-      <Board4Players /> */}
     </div>
   )
 }
