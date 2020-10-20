f71fc56132f9e27b31afcbbcdd743aefee7458ba
unknown <izra.arzi@gmail.com>
2020-10-20 21:49:04 +0700

    finished basic logic for the game

--- server/controller/socket.io.js
+++ server/controller/socket.io.js
@@ -206,8 +206,6 @@ module.exports = function (io) {
       };
 
       boards.push(coordinates);
-      attackers[code] = []
-      attackers[code].push()
 
       if (boards.length === lobby.players.length) {
         console.log("All players have placed their ships");
@@ -226,24 +224,36 @@ module.exports = function (io) {
     });
 
     socket.on("resolveAttacks", async (bombs) => {
-      console.log(bombs, "ajkshc");
+      let advanceFlag = false;
       const code = Object.keys(socket.rooms)[1];
       const lobby = await db.collection("lobby").findOne({ code });
       let lastBoard = lobby.boardLogs[lobby.boardLogs.length - 1];
 
-      attackers.forEach((attack) => {
+      if (!attackers[code]) {
+        attackers[code] = [];
+      }
+      attackers[code].push({ socketId: socket.id, underFire: [] });
+
+      attackers[code].forEach((attack) => {
         bombs.forEach((bomb) => {
-          if (attack.socketId === Object.keys(bomb)[0]) {
-            attack.underFire.push(bomb[Object.keys(bomb)[0]]);
+          if (attack.socketId === bomb.socketId) {
+            attack.underFire.push(bomb.coordinate);
           }
         });
       });
 
+      attackers[code].forEach((socketDamage, index) => {
+        if (
+          socketDamage.underFire.length === 1 &&
+          index === attackers[code].length - 1
+        ) {
+          advanceFlag = true;
+        }
+      });
 
-      if (attackers.length === lobby.players.length) {
-        console.log(attackers, "ini attackers yang udah semua");
+      if (advanceFlag) {
         lastBoard = lastBoard.map((boardOfSocket) => {
-          attackers.forEach((attack) => {
+          attackers[code].forEach((attack) => {
             if (boardOfSocket.socketId === attack.socketId) {
               attack.underFire.forEach((coor) => {
                 boardOfSocket.coordinates.attacked.push(coor);
@@ -252,12 +262,42 @@ module.exports = function (io) {
           });
           return boardOfSocket;
         });
-        console.log(lastBoard, "ini last board nya");
+        io.to(code).emit('resolving')
+
+        // Check every players of any sunk ship
+        lastBoard.forEach(player => {
+          let {coordinates} = player
+          let {attacked, ships} = coordinates
+          ships.forEach(ship => {
+            ship.isAlive = true
+            ship.coordinates.forEach(coordinate => {
+              attacked.forEach(point => {
+                if (`${point}` === `${coordinate}`) {
+                  ship.isAlive = false
+                }
+              })
+            })
+          })
+        })
+
+        await db.collection('lobby').updateOne(
+          { code },
+          {
+            $push: {
+              boardLogs: lastBoard
+            }
+          }
+        )
+        
+        io.to(code).emit('resolved', lastBoard)
+        attackers[code] = [];
       }
     });
 
     socket.on("nukeDatabase", async () => {
       await db.collection("lobby").deleteMany({});
+      attackers = {};
+      boards = [];
     });
 
     socket.on("byebye", () => {
